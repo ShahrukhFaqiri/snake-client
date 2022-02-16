0ba255633c24924d5c7f1b4fb5482effa710e27b
Sha Faqiri <shahrukh.faqiri@gmail.com>
2022-02-16 22:42:57 +0000

    added key input handler to play.js

--- client.js
+++ client.js
@@ -2,6 +2,7 @@ const net = require('net');
 
 //Connecting to the server, retrievs data from the server.
 const connect = () => {
+
   const conn = net.createConnection({
     host: '165.227.47.243',
     port: 50541,
@@ -16,8 +17,8 @@ const connect = () => {
   conn.on('data', (data) => {
     console.log('Server response:', data);
   });
-
   conn.setEncoding('utf8');
+  
   return conn;
 };
 
--- play.js
+++ play.js
@@ -1,4 +1,21 @@
 const { connect } = require('./client');
 
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.on('data', handleUserInput);
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = (key) => {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+setupInput();
+
 console.log('Connecting ...');
 connect();
